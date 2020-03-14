08d7ae82512bc7f50014a95228f5abaad39d4799
Andre Arruda <adr_hb@hotmail.com>
2020-03-13 22:55:03 -0300

    Feito script do morcego, no momento ele segue, morre porem nao causa dano.

--- Morcego.cs
+++ Morcego.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Morcego : MonoBehaviour {
 
-    private float _vidaTotal;
+    private float _vidaTotal = 2;
     private float _velocidadeMovimento = 5.0f;
     private float _alcanceVisao = 10f;
+    
 
     private bool _vendoPlayer;
     private bool _verPlayer;
@@ -22,14 +22,17 @@ public class Morcego : MonoBehaviour {
     private Rigidbody2D _rbMorcego;
     private CircleCollider2D _morcegoCollider;
 
+    private Animator _morcegoAnimacao;
+
     private CharPlayer _charPlayer;
+    private CameraShake _cameraShake;
 
 
-    public float VidaAtual { get; set; }
+    public float VidaAtual { get; private set; }
 
     public bool EstaVoando { get; set; }
 
-    public AudioSource[] _morcegoSons { get; set; }
+    public AudioSource[] MorcegoSons { get; set; }
 
     void Awake() {
         _barraVida = Instantiate(_barraVidaPrefab, FindObjectOfType<Canvas>().transform.Find("barrasDeVida").transform).GetComponent<Image>();
@@ -37,13 +40,41 @@ public class Morcego : MonoBehaviour {
 
         _rbMorcego = GetComponent<Rigidbody2D>();
         _morcegoCollider = GetComponent<CircleCollider2D>();
-        _charPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<CharPlayer>();
 
+        _morcegoAnimacao = GetComponent<Animator>();
+
+        _charPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<CharPlayer>();
+        _cameraShake = GameObject.Find("CameraShake").GetComponent<CameraShake>();
+
+        MorcegoSons = transform.Find("morcegoSons").GetComponentsInChildren<AudioSource>();
 
     }
 
+    void Start() {
+        VidaAtual = _vidaTotal;
+    }
+
     void FixedUpdate() {
-        _rbMorcego.MovePosition(Vector2.MoveTowards(transform.position, _charPlayer.gameObject.transform.position, _velocidadeMovimento));
+        _rbMorcego.MovePosition(Vector2.MoveTowards(transform.position, _charPlayer.gameObject.transform.position, _velocidadeMovimento * Time.fixedDeltaTime));
+        Interface();
+    }
+
+    public void InfligirDano(int danoInfligido) {
+        VidaAtual -= danoInfligido;
+        _morcegoAnimacao.SetTrigger("recebendoDano");
+        MorcegoSons[4].Play();
+
+        if(VidaAtual <= 0){
+            Morrer();
+        }
+    }
+
+    void Interface() {
+        float altura = 0.5f;
+
+        //Barra de vida posição
+        _barraVida.transform.position = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, altura, 0));
+        _barraVidaCheia.fillAmount = VidaAtual / _vidaTotal;
 
     }
 
@@ -63,11 +94,40 @@ public class Morcego : MonoBehaviour {
 
     }
 
+    void Morrer() {
+        transform.position = new Vector2(transform.position.x, transform.position.y);
+        MorcegoSons[0].Stop();
+        MorcegoSons[3].Play();
+        MorcegoSons[2].Play();
+
+        _charPlayer.AdicionarAlmas(1);
+        _cameraShake.Shake(0.1f, 0.1f);
+        //MorcegoSons.SetBool("estaMorrendo", true);
+
+        GetComponent<SpriteRenderer>().sortingOrder = 3;
+
+       
+        Destroy(_barraVida.gameObject);
+        
+
+        Destroy(GetComponent<CircleCollider2D>());
+        Destroy(GetComponent<Rigidbody2D>());
+        Destroy(this);
+    }
 
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(new Vector2(transform.position.x, transform.position.y), _alcanceVisao);
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Bala")) {
+            _verPlayer = true;
+            InfligirDano(_charPlayer.BalaPistolDano);
+            Destroy(collision.gameObject);
+            
+        }
     }
 
 }
