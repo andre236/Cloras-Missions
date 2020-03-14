d7c3f87c9aac4adff9ad6b7bd7000b3dffa3c9a9
Andre Arruda <adr_hb@hotmail.com>
2020-03-14 07:41:19 -0300

    Adicionado as Layers permitidas pela visao, perseguicao e a morte.

--- Morcego.cs
+++ Morcego.cs
@@ -6,23 +6,22 @@ public class Morcego : MonoBehaviour {
 
     private float _vidaTotal = 2;
     private float _velocidadeMovimento = 5.0f;
-    private float _alcanceVisao = 10f;
+    private float _alcanceVisao = 20f;
     
-
     private bool _vendoPlayer;
-    private bool _verPlayer;
     
-
     [SerializeField]
     private GameObject _barraVidaPrefab;
+    
+
+    private Rigidbody2D _rbMorcego;
+    private BoxCollider2D _morcegoCollider;
+    private Animator _morcegoAnimacao;
+    [SerializeField]
+    private LayerMask _layersPermitidas;
 
     private Image _barraVida;
     private Image _barraVidaCheia;
-
-    private Rigidbody2D _rbMorcego;
-    private CircleCollider2D _morcegoCollider;
-
-    private Animator _morcegoAnimacao;
 
     private CharPlayer _charPlayer;
     private CameraShake _cameraShake;
@@ -35,27 +34,29 @@ public class Morcego : MonoBehaviour {
     public AudioSource[] MorcegoSons { get; set; }
 
     void Awake() {
+        
+        _rbMorcego = GetComponent<Rigidbody2D>();
+        _morcegoCollider = GetComponent<BoxCollider2D>();
+        _morcegoAnimacao = GetComponent<Animator>();
+
+        MorcegoSons = transform.Find("morcegoSons").GetComponentsInChildren<AudioSource>();
+
         _barraVida = Instantiate(_barraVidaPrefab, FindObjectOfType<Canvas>().transform.Find("barrasDeVida").transform).GetComponent<Image>();
         _barraVidaCheia = new List<Image>(_barraVida.GetComponentsInChildren<Image>()).Find(img => img != _barraVida);
-
-        _rbMorcego = GetComponent<Rigidbody2D>();
-        _morcegoCollider = GetComponent<CircleCollider2D>();
-
-        _morcegoAnimacao = GetComponent<Animator>();
 
         _charPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<CharPlayer>();
         _cameraShake = GameObject.Find("CameraShake").GetComponent<CameraShake>();
 
-        MorcegoSons = transform.Find("morcegoSons").GetComponentsInChildren<AudioSource>();
 
     }
 
     void Start() {
         VidaAtual = _vidaTotal;
+
     }
 
     void FixedUpdate() {
-        _rbMorcego.MovePosition(Vector2.MoveTowards(transform.position, _charPlayer.gameObject.transform.position, _velocidadeMovimento * Time.fixedDeltaTime));
+        PerserguirPlayer();
         Interface();
     }
 
@@ -79,20 +80,36 @@ public class Morcego : MonoBehaviour {
     }
 
     void PerserguirPlayer() {
-        var diferencaParaJogadorX = _charPlayer.gameObject.transform.position.x - transform.position.x;
-        var diferencaParaJogadorY = _charPlayer.gameObject.transform.position.y - transform.position.y;
-        _verPlayer = Mathf.Abs(diferencaParaJogadorX) < _alcanceVisao && Mathf.Abs(diferencaParaJogadorY) < _alcanceVisao;
 
-        
-        _vendoPlayer = (_verPlayer == true) ? _vendoPlayer = true : _vendoPlayer = false;
+        Vector2 direcao = new Vector2(_charPlayer.gameObject.transform.position.x - transform.position.x, _charPlayer.transform.position.y - transform.position.y);
 
+        RaycastHit2D visaoInfo = Physics2D.Raycast(transform.position, direcao, _alcanceVisao, _layersPermitidas);
         
-        if (_vendoPlayer) {
-            _rbMorcego.MovePosition(Vector2.MoveTowards(transform.position, _charPlayer.gameObject.transform.position, _velocidadeMovimento));
+        if (visaoInfo.collider) {
+            Debug.DrawLine(transform.position, visaoInfo.point, Color.green);
+
+            //# Caso veja o player.
+            if (visaoInfo.collider.CompareTag("Player")) {
+                Debug.DrawLine(transform.position, visaoInfo.point, Color.red);
+                _vendoPlayer = true;
+            }
 
         }
 
+        if (_vendoPlayer) {
+            _rbMorcego.MovePosition(Vector2.MoveTowards(transform.position, _charPlayer.transform.position, _velocidadeMovimento * Time.fixedDeltaTime));
+        }
+
+        if (direcao.x < 0) {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
     }
+
+    
 
     void Morrer() {
         transform.position = new Vector2(transform.position.x, transform.position.y);
@@ -115,15 +132,11 @@ public class Morcego : MonoBehaviour {
         Destroy(this);
     }
 
-    private void OnDrawGizmos() {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(new Vector2(transform.position.x, transform.position.y), _alcanceVisao);
-
-    }
+ 
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Bala")) {
-            _verPlayer = true;
+            _vendoPlayer = true;
             InfligirDano(_charPlayer.BalaPistolDano);
             Destroy(collision.gameObject);
             
